6248f2cff9eef0d47e713a6cd69381429f3b61e1
jmewader <smelw@list.ru>
2024-04-10 13:23:16 +0300

    fix logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,12 +4,13 @@ import LogoImage from "~src/images/logo.svg";
 import LogoGreyImage from "~src/images/logoV2.svg";
 
 const StyledHeaderLogo = styled.div`
+  display: flex;
+  align-items: center;
   width: 90px;
-  height: 70px;
+  height: 50px;
 
   @media ${uiKitBreakpoints.maxWidth.L} {
     width: 70px;
-    height: 50px;
   }
 `;
 
